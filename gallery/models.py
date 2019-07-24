e8b6d3d20992b3fc3d94acfeb96a2467d62b9736
fcs <1501071509@qq.com>
2019-07-24 11:38:11 +0800

    增加app作品的标题和上次图片

--- gallery/models.py
+++ gallery/models.py
@@ -4,4 +4,9 @@ from django.db import models
 
 
 class Gallery(models.Model):
-	miaoshu = models.CharField(max_length=100)
+    miaoshu = models.CharField(default='在这里写作品的描述', max_length=100)
+    image = models.ImageField(default='default.png', upload_to='images/')
+    title = models.CharField(default='作品标题', max_length=50)
+
+    def __str__(self):
+    	return self.title
